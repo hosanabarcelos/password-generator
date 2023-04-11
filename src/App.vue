f9ff8b4bc9cdc3e6a6d464669787a7b2801abacb
Hosana <hosanabarceloscandido@gmail.com>
2023-04-10 23:51:39 -0300

    feat: add main page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
-<p>Vue</p>
+<div class="main__container">
+    <img src="@/assets/banner.png">
+    <GeneratePasswordForm />
+</div>
 </template>
 
 <script>
+import GeneratePasswordForm from '@/components/GeneratePasswordForm.vue';
 
 export default {
-  name: 'App',
+  components: {
+    GeneratePasswordForm
+  }
 }
+
 </script>
 
-<style>
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+    font-family: 'Roboto', sans-serif;
+    font-size: x-large;
+}
+.main__container {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #201E30;
+    gap: 80px;
+}
+img {
+    width: 380px;
+}
 </style>
